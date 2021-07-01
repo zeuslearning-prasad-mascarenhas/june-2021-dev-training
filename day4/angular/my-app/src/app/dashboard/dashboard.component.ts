2d9eb85a0d162db3e07e107fc291ceeb4372e97c
Prasad0704 <prasadrspm07@gmail.com>
2021-07-01 14:38:28 +0530

    angular 2

--- day4/angular/my-app/src/app/dashboard/dashboard.component.ts
+++ day4/angular/my-app/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
+import data from '../../json/dash.json';
+interface Dashdata {
+  title: String;
+  image: String;
+  fav: boolean;
+  subjectsection: {
+    subject: String;
+    grade: String;
+    plus: String;
+  };
+  unitsection: {
+    istrue: boolean;
+    unit: String;
+    lesson: String;
+    topic: String;
+  };
+  dropdown: {
+    istrue: boolean;
+    dropcontent: String[];
+    student: String;
+    date: String;
+  };
+  option: {
+    image1: boolean;
+    image2: boolean;
+    image3: boolean;
+    image4: boolean;
+  };
+}
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  dashdata: Dashdata[] = data;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
